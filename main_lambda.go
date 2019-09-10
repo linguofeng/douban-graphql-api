a09639af5e4e6bc4edf465db2ebbeb140f354949
林国锋 <linguofeng@msn.com>
2019-09-10 19:25:11 +0800

    feat: main_lambda

--- main_lambda.go
+++ main_lambda.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func hello() (string, error) {
-	return "Hello ƛ!", nil
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello AWS Lambda and Netlify",
+	}, nil
 }
 
 func main() {
-	lambda.Start(hello)
+	lambda.Start(handler)
 }
