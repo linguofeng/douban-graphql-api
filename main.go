b67047f06e614147d13cf124c80a7e2554d0fc20
林国锋 <linguofeng@msn.com>
2019-09-10 20:19:29 +0800

    feat: lambda graphql handle

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/graphql-go/graphql"
 	"github.com/labstack/echo/v4"
@@ -12,6 +14,50 @@ import (
 )
 
 var isLambda = "false"
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	acceptHeader := req.Headers["Accept"]
+	query := req.QueryStringParameters["query"]
+	var result = []byte("")
+	if query != "" {
+		operationName := req.QueryStringParameters["operationName"]
+		variablesString := req.QueryStringParameters["variables"]
+		variables := make(map[string]interface{})
+		_ = json.Unmarshal([]byte(variablesString), &variables)
+		if strings.Contains(acceptHeader, "text/html") {
+			result = []byte("graphiql")
+		} else {
+			result, _ = json.Marshal(graphql.Do(graphql.Params{
+				Schema:         schema.New(),
+				RequestString:  query,
+				VariableValues: variables,
+				OperationName:  operationName,
+			}))
+		}
+	} else {
+		body := new(struct {
+			Query         string                 `json:"query"`
+			Variables     map[string]interface{} `json:"variables"`
+			OperationName string                 `json:"operationName"`
+		})
+		_ = json.Unmarshal([]byte(req.Body), &body)
+		result, _ = json.Marshal(graphql.Do(graphql.Params{
+			Schema:         schema.New(),
+			RequestString:  body.Query,
+			VariableValues: body.Variables,
+			OperationName:  body.OperationName,
+		}))
+	}
+	return events.APIGatewayProxyResponse{
+		Body:       string(result),
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
+			"Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		},
+	}, nil
+}
 
 func main() {
 	app := echo.New()
@@ -63,7 +109,7 @@ func main() {
 	})
 
 	if isLambda == "true" {
-		lambda.Start((&helpers.LambdaAdapter{Echo: app}).Handler)
+		lambda.Start(handler)
 	} else {
 		app.Start("localhost:1234")
 	}
